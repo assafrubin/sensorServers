e7ba26a929c0390f9da9f1e2fc444d45d5434a51
Assaf Rubin <assafrubin@dynmaicyield.com>
2016-05-20 18:43:50 +0300

    mysql config

--- src/orm/sequelize.js
+++ src/orm/sequelize.js
@@ -1,9 +1,11 @@
 /**
  * Created by Assaf on 5/14/2016.
  */
+var P = require('autoresolve');
+var config = require(P('config/config')).config;
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize('sensors_dev', 'root', 'root', {
-  host: 'localhost',
+var sequelize = new Sequelize('sensors_dev', config.mysql.username, config.mysql.password, {
+  host: config.mysql.host,
   dialect: 'mysql',
 
   pool: {
